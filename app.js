fb1055d2e3a5ebf91c018ccde53f4463d6684b89
Constantinos Davanos <cdavanos@gmail.com>
2018-06-10 21:11:34 -0400

    Adding user model, setting up regist/login route

--- app.js
+++ app.js
@@ -1,27 +1,32 @@
-const express = require("express");
-const mongoose = require("mongoose");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+// Routes
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const posts = require('./routes/api/posts');
 
 const app = express();
 
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
   .connect(db)
-  .then(() => console.log("MongoDB Connected"))
+  .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
 
-// Routes
-const users = require("./routes/api/users");
-const profile = require("./routes/api/profile");
-const posts = require("./routes/api/posts");
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/posts', posts);
 
-app.use("/api/users", users);
-app.use("/api/profile", profile);
-app.use("/api/posts", posts);
-
-app.get("/", (req, res) => res.send("hello"));
+app.get('/', (req, res) => res.send('hello'));
 
 const port = process.env.PORT || 5000;
 
--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://cdavanos:gen1khpa0k@ds253840.mlab.com:53840/nutrition"
+  mongoURI: 'mongodb://cdavanos:gen1khpa0k@ds253840.mlab.com:53840/nutrition',
+  secret: 'secret'
 };
